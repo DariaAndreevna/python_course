9eeb24de3deefaf0e11bae1009761c50b3a7cc40
daria.r <dariaandreevna1206@gmail.com>
2024-08-18 17:17:28 +0200

    Address the comment

--- fifth_lesson/hometask_5.3.py
+++ fifth_lesson/hometask_5.3.py
@@ -3,11 +3,11 @@ import string
 slogan = input("Enter your string:")
 slogan = str(slogan)
 
-my_hashtag = slogan.title()
-my_hashtag = my_hashtag.replace(" ", "")
+my_hashtag = slogan
 for p in string.punctuation:
     my_hashtag = my_hashtag.replace(p, "")
-
+my_hashtag = my_hashtag.title()
+my_hashtag = my_hashtag.replace(" ", "")
 my_hashtag = "#" + my_hashtag
 my_hashtag = my_hashtag[:140]
 
